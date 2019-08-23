164130b3e569a2bb1cdf747bb2fce2888123562e
AmyEtcetera <AmyEtcetera@users.noreply.github.com>
2019-08-23 00:15:53 -0400

    Copy edit index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default () => (
 	<div style={{ color: `gray`, fontFamily: `Arial`, fontSize: `3rem` }}>
 		<Link to="/contact/">Contact</Link> | <Link to="/about/">About</Link>
-		<Header headerText="oops" />
+		<Header headerText="It's a test, obvi" />
 		<img src="https://picsum.photos/400/200" alt="random landscape or other" />		
 		<p>Hello world! I wonder how you are doing.</p>
 		<p><a href="https://www.google.com/">Link to something off this site</a> is a regular anchor tag.</p>	</div>
